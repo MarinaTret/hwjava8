3dc8a3de371a25f97f52f9cad96bf8731d3210f9
MarinaTret <jiharka88@mail.ru>
2024-01-24 11:48:31 +0300

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; // паcпорт
+    public String patronymic; // отчество
+    public String phone; //телефон
+    public String surname; //фамилия
+    public boolean subscription; //согласен ли
+    // пользователь получать новости о скидках в виде sms
+    public FormDate birthday;
+
+
+}
